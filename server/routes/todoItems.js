6ae1fb2e55d7ff04a8b99fd13dec08f09d915a5d
Sergi <scsanz87@gmail.com>
2023-04-13 13:29:34 +0200

    2cond route created

--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -8,8 +8,8 @@ const TodoItemModel = require('../models/todoItems');
 
 //CREAMOS LA PRIMERA RUTA -- AÑADIREMOS WISH ITEMS A LA DATABASE
 
-router.post('/api/item', async(req, res)=> {
-    try{
+router.post('/api/item', async (req, res) => {
+    try {
         const newItem = new TodoItemModel({
             item: req.body.item
         })
@@ -17,17 +17,32 @@ router.post('/api/item', async(req, res)=> {
         const saveItem = await newItem.save()
         res.status(200).json('Item added Successfully.');
 
-        }catch(err){
-            res.json(err);
-        }
+    } catch (err) {
+        res.json(err);
+    }
 
 
-    })
+})
+
+//CREAMOS UNA SEGUNDA RUTA --OBTENEMOS DATA DE DATABASE
+
+router.get('/api/items', async (req, res) => {
+
+    try{
+        const allTodoItems = await TodoItemModel.find({});
+        resizeTo.status(200).json(allTodoItems)
+        }catch (err) {
+        res.json(err);
+
+    }
 
 
-    //EXPORT ROUTER
+})
 
-    module.exports = router;
+
+//EXPORT ROUTER
+
+module.exports = router;
 
 
 
