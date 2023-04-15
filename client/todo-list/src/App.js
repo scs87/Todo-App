dc47a0461b1aa62c55b1a2320c7b0e3979786fa3
Sergi <scsanz87@gmail.com>
2023-04-15 11:23:51 +0200

    created form and started app-srtyles

--- client/todo-list/src/App.js
+++ client/todo-list/src/App.js
@@ -3,25 +3,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Todo List</h1>
       <form>
         <input type="text" placeholder='Add Todo Item'></input>
         <button type="submit">Añadir</button>
       </form>
       <div className="todo-listItems"></div>
       <div className="todo-item">
-        <p>This is the item 1</p>
-        <button>Update</button>
-        <button>Borrar</button>
+        <p className="item-content">This is the item 1</p>
+        <button className="update-item">Update</button>
+        <button className="delete-item">Borrar</button>
       </div>
       <div className="todo-item">
-        <p>This is the item 2</p>
-        <button>Update</button>
-        <button>Borrar</button>
+        <p className="item-content">This is the item 2</p>
+        <button className="update-item">Update</button>
+        <button className="delete-item">Borrar</button>
       </div>
       <div className="todo-item">
-        <p>This is the item 3</p>
-        <button>Update</button>
-        <button>Borrar</button>
+        <p className="item-content">This is the item 3</p>
+        <button className="update-item">Update</button>
+        <button className="delete-item">Borrar</button>
       </div>
     </div>
   );
